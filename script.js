eba4daf105396e631e45c248a84b48808ae6d3ce
CarlErikssonSkogh <116281397+CarlErikssonSkogh@users.noreply.github.com>
2023-05-07 21:01:30 +0200

    added comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*funktion för sidebar */
 const openSidebar = () => {
     document.getElementById("mySidebar").style.width = "30vh";
     document.getElementById("main").style.marginLeft = "30vh";
@@ -10,6 +10,38 @@ const closeSidebar = () => {
 };
 
 
+/*funktion för sökning*/
+function searchFunction() {
+    
+                var li = document.getElementsByTagName('li');
+
+            for (i = 0; i < li.length; i++) {
+
+                 li[i].style.display = "none";
+
+             }
+    var input, filter, ul, li, a, i, txtValue, test;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    test = document.getElementById("test");
+
+    for (i = 0; i < li.length; i++) {
+        if(filter==""){
+            li[i].style.display = "none";
+        }
+        else{
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+    }
+}
+}
 
 
 
